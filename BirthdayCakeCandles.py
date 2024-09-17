f3fc26f3bd04e777db823e36b782496ea82f2bd7
Soumik1410 <soumik1410code@gmail.com>
2024-09-17 11:15:39 +0530

    Create BirthdayCakeCandles.py

---
+++ BirthdayCakeCandles.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthdayCakeCandles' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY candles as parameter.
+#
+
+def birthdayCakeCandles(candles):
+    # Write your code here
+    max = -1
+    count = 0
+    
+    for candle in candles:
+        if candle == max:
+            count = count + 1
+            
+        if candle > max:
+            max = candle
+            count = 1
+    
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    candles_count = int(input().strip())
+
+    candles = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(candles)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
