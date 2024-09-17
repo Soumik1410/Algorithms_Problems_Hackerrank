9b73faab5e8fbcb0fb85cc786e4ac654855d14a3
Soumik1410 <soumik1410code@gmail.com>
2024-09-17 11:13:06 +0530

    Create AbsoluteDifferenceOfDiagonalsOfSquareMatrix.py

---
+++ AbsoluteDifferenceOfDiagonalsOfSquareMatrix.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    # Write your code here
+    i = 0
+    leftsum = 0
+    rightsum = 0
+    
+    for row in arr:
+        leftsum = leftsum + row[i]
+        rightsum = rightsum + row[len(row) - 1 - i]
+        i = i + 1
+    return abs(leftsum - rightsum)    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
